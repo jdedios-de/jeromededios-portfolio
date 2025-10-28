33842e7d49567d7aa288d60e4d1151c6f6179773
Jerome de Dios <jeromededios.de@proton.me>
2025-10-28 09:49:45 +0100

    fix(main-page): added informations

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/mstark/ghq/github.com/webeet-io/_onboarding_portfolio_template/src/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/jdedios/projects/personal-project/portfolio/jeromededios-portfolio/src/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/mstark/ghq/github.com/webeet-io/_onboarding_portfolio_template/src/app/layout.tsx
+// File: /Users/jdedios/projects/personal-project/portfolio/jeromededios-portfolio/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
